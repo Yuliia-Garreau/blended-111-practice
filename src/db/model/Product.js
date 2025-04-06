37c9ff02fe7a208e47bfa04720374d2cb48f91a2
Olena_Chovgun <olenkachovgun@gmail.com>
2025-04-06 15:10:03 +0300

    mode

---
+++ src/db/model/Product.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+export const ProductModel = model('product', productSchema);
